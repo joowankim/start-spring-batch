f3ff365a2fb6ae9d0f20fa6a072d6bef5f6642cb
Joowan Kim <rlawndhks217@gmail.com>
2021-04-17 00:41:41 +0900

    first batch

---
+++ src/test/java/io/joowan/batch_demo/BatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.joowan.batch_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
